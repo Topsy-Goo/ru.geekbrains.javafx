34b81620c4bad5a976e90959ffffe8d888dcb6e8
Topsy-Goo <topsygoo@yandex.ru>
2021-02-27 00:25:43 +0300

    java2 / урок 4 (javafx)
    Наброски, сделанные по материалам лекции. Не годятся для помещения в pull request.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
-public class Controller {
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+
+    @FXML TextArea someTextAreaID; //< имя переменной совпадает с fx:id соответствующего элемента управления (тут
+    // нужно следить за тем, чтобы импорт сгенерировался из правильного пакета -- scene.control, а не awt)
+
+    @FXML TextField someTextFieldID;
+    @FXML RadioButton answer1, answer2, answer3, answer4; //Можно ссылаться на каждую радио-кнопку в отдельности, …
+    @FXML ToggleGroup answers;  //  …а можно ссылаться на их группу.
+
+
+    @Override public void initialize (URL location, ResourceBundle resources)
+    {
+        someTextAreaID.setText ("Пройдите короткий опрос");
+        answer1.setText ("Red");
+        answer2.setText ("Green");
+        answer3.setText ("Blue");
+        answer4.setText ("Yellow");
+    }
+
+
+    public void clickGetCCNumber (ActionEvent actionEvent) // входные параметры необязательны
+    {
+        String strCCNumber = someTextFieldID.getText();
+        someTextFieldID.clear();
+        //if (someTextFieldID.getLength() > 0)
+            someTextAreaID.setText ("Ваш номер кредитной карты : "+strCCNumber+".");
+        someTextAreaID.appendText ("\nСпасибо!");
+        someTextAreaID.appendText ("\nНаш менеджер свяжется с Вами совсем скоро.");
+    }
+
+    public void tryToAnswer ()
+    {
+        //обрабатываем нажатия на радио-кнопки. Если запрашивать группу, то делать нужно так:
+        String s = ((RadioButton)answers.getSelectedToggle()).getText();
+        //...
+    }
+
+}// class Controller
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    @Override public void start (Stage primaryStage) throws Exception
+    {
+    //создаём окно:
+        Parent root = FXMLLoader.load (getClass ().getResource ("sample.fxml"));
+        primaryStage.setTitle (" Фонд «Озеленение Луны»");
+    //создали «сцену» (внутреннее наполнение клиентской области окна) и привязали её к окну:
+        primaryStage.setScene (new Scene (root, 700, 475));
+    //показали окно:
+        primaryStage.show ();
+
+    //
+        Alert alert = new Alert (Alert.AlertType.CONFIRMATION, "Здрасьте!", ButtonType.APPLY);
+        alert.showAndWait(); //< похоже, то же что и show(), но приостанавливает работу программы (для наглядности
+        // можно поменять местами этот вызов с вызовом show(), используемым ниже.)
+        alert = new Alert (Alert.AlertType.ERROR, "Здрасьте!", ButtonType.OK); // ButtonType.CLOSE
+        alert.show();
+
+    //(Окном управляет сласс Controller.)
+
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main (String[] args)
+    {
+        launch (args);
     }
 }
